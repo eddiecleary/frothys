57b5f02ce17bf62663f70a9c5a7dae7d92a76bc0
Eddie Cleary <eddie@eddiecleary.com>
2020-10-09 07:17:23 +0800

    remove layout component from 404

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
-import Layout from '../components/Layout'
-
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-  </Layout>
+  <h1>NOT FOUND</h1>
 )
 
 export default NotFoundPage
